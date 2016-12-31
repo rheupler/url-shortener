92f687a4b93a52b96b92b53453ff3feab21015eb
Ryan <rheupler@gmail.com>
2016-12-30 22:23:44 -0500

    gets rid of not working saving function

--- models/url.js
+++ models/url.js
@@ -12,17 +12,5 @@ var urlSchema = Schema({
   created_at: Date
 })
 
-urlSchema.pre('save', function(next){
-  var doc = this;
-  // find the url_count and increment it by 1
-  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
-      if (error)
-          return next(error);
-      // set the _id of the urls collection to the incremented value of the counter
-      doc._id = counter.seq;
-      doc.created_at = new Date();
-      next();
-  });
-});
 
 var counter = mongoose.model('counter', CounterSchema);
